c8c483abd116f4a8fa5b63145adfdf2c37e881c5
Jia Xi Lin <000460612@student.vcc.ca>
2023-01-19 12:50:23 -0800

    update

---
+++ week3/HeaderAndClasses/myheader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace cstp1205
+{
+	void myPrint(string msg);
+
+	void getUserInfo();
+
+}
+namespace cstp1104
+{
+	void myPrint(string msg);
+}
+
+
